b8278fbe12b32281347bdd0d921c850ac1150b63
MexicanoT18 <lucas.fra.oli18@gmail.com>
2017-04-11 22:10:18 -0300

    contest CF 100247 completo

--- codeforces/gym/100247/G.cpp
+++ codeforces/gym/100247/G.cpp
@@ -2,40 +2,16 @@
 using namespace std;
 
 typedef long long ll;
-typedef pair<ll, ll> ii;
-
-ll gcd(ll a, ll b){
-	if (b == 0) return a;
-	else return gcd(b, a%b);
-}
-
-vector<ii> factors(ll n)
-{
-	vector<ii> ans;
-	for(int i=1; i*i <= n; i++){
-		if (n%i == 0){
-			ans.push_back(ii(i, n/i));
-		}
-	}
-	return ans;
-}
 
 int main()
 {
-	int n;
+	ll n;
 	scanf("%I64d", &n);
-	vector<ii> pos = factors(n);
 	bool possible = false;
-	for(int i=0; i<(int)pos.size() && !possible; i++){
-		int a = pos[i].first;
-		int b = pos[i].second;
-		if ((b-a)%a != 0) continue;
-		int k2 = (b-a)/a;
-		for(int k=sqrt(k2); k*k <= k2; k++){
-			if (k*k == k2){
-				break;
-			}
-		}
+	for(ll i=0, j; i*i<=n; i++)
+	{
+		j = sqrt(n - i*i);
+		if (j*j + i*i == n) possible = true;
 	}
 	if (possible) printf("Yes\n");
 	else printf("No\n");
