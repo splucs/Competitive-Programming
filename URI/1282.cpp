a7ebb341bb211cc26fbfe310df5f911be1ef659b
splucs <lucas.fra.oli18@gmail.com>
2018-06-21 13:10:12 -0300

    updated uri 1282

--- URI/1282.cpp
+++ URI/1282.cpp
@@ -4,46 +4,38 @@
 #define MAXN 1009
 using namespace std;
 
-typedef long long int ll;
+typedef long long ll;
 
 class CHTrick {
 private:
 	vector<ll> m, n;
 	vector<double> p;
+    vector<int> ids;
 	bool maxCH; int i;
 public:
 	CHTrick(bool mxch) { maxCH = mxch; i = 0; }
-	void clear() { m.clear(); n.clear(); p.clear(); }
+	void clear() { m.clear(); n.clear(); p.clear(); ids.clear(); }
 	double inter(double nm, double nn, double lm, double ln) {
 		return (ln - nn) / (nm - lm);
 	}
-	void push(ll nm, ll nn) {
+	void push(ll nm, ll nn, int id) {
+        //printf("push line %lldx + %lld\n", nm, nn);
 		while (!p.empty()) {
             if (nm == m.back() && maxCH && nn <= n.back()) return;
             if (nm == m.back() && !maxCH && nn >= n.back()) return;
             double x = inter(nm, nn, m.back(), n.back());
 			if (nm != m.back() && p.back() < x) break;
-			m.pop_back(); n.pop_back(); p.pop_back();
+			m.pop_back(); n.pop_back(); p.pop_back(); ids.pop_back();
 		}
 		p.push_back(p.empty() ? -INF : inter(nm, nn, m.back(), n.back()));
-		m.push_back(nm); n.push_back(nn);
+		m.push_back(nm); n.push_back(nn); ids.push_back(id);
         if (i >= p.size()) i = p.size()-1;
 	}
-	ll query(ll x) {
-		if (p.empty()) return (maxCH ? -1 : 1)*INF;
-		ll r = p.size() - 1, l = 0, mid;
-		if (x >= p[r]) return m[r] * x + n[r];
-		while (r > l + 1) {
-			mid = (r + l) / 2;
-			if (x < p[mid]) r = mid;
-			else l = mid;
-		}
-		return m[l] * x + n[l];
-	}
-    ll query_q(ll x) {
+    pair<ll, int> query_q(ll x) {
         while (p[i] > x) i--;
         while(i+1 < p.size() && p[i+1] <= x) i++;
-        return m[i] * x + n[i];
+        //printf("ans for x = %lld: %lldx + %lld = %lld\n", x, m[i], n[i], m[i] * x + n[i]);
+        return make_pair(m[i] * x + n[i], ids[i]);
     }
 };
 
@@ -51,28 +43,48 @@ public:
  * URI 1282
  */
 
-ll w[MAXN], x[MAXN], A[MAXN], B[MAXN], dp[MAXN][MAXN];
+ll w[MAXN], x[MAXN], A[MAXN], B[MAXN];
+ll dp[MAXN];
+int opt[MAXN], N, K;
+
+CHTrick cht(false);
+
+int computeDp(ll C) {
+    cht.clear();
+    dp[0] = 0;
+    opt[0] = 0;
+    for(int i = 1; i <= N; i++) {
+        cht.push(-A[i-1], dp[i-1] + B[i-1], i-1);
+        pair<ll, int> cur = cht.query_q(x[i]);
+        dp[i] = C + x[i]*A[i] - B[i] + cur.first;
+        opt[i] = 1 + opt[cur.second];
+    }
+    return opt[N];
+}
+
+ll computeC() {
+    ll lo = 0;
+    ll hi = 0x3f3f3f3f3f3f3f3f;
+    while(hi > lo + 1) {
+        ll C = (hi + lo) / 2;
+        int k = computeDp(C);
+        if (k > K) lo = C;
+        else hi = C;
+    }
+    return hi;
+}
 
 int main(){
-    int N, K;
-    CHTrick cht(false);
-    while(scanf("%d %d", &N, &K)!=EOF){
-        for(int i=0; i<N; i++){
-            scanf("%lld %lld", x+i, w+i);
-            A[i] = w[i] + (i>0 ? A[i-1] : 0);
-            B[i] = w[i]*x[i] + (i>0 ? B[i-1] : 0);
-            dp[i][1] = x[i]*A[i] - B[i];
+    while (scanf("%d %d", &N, &K) != EOF){
+        A[0] = B[0] = 0;
+        for (int i=1; i<=N; i++) {
+            scanf("%lld %lld", &x[i], &w[i]);
+            A[i] = w[i] + A[i-1];
+            B[i] = w[i]*x[i] + B[i-1];
         }
-        for(int k=2; k<=K; k++){
-            dp[0][k] = 0;
-            cht.clear();
-            for(int i=1; i<N; i++){
-                cht.push(-A[i-1], dp[i-1][k-1]+B[i-1]);
-                //dp[i][k] = x[i]*A[i] - B[i] + cht.query(x[i]);
-                dp[i][k] = x[i]*A[i] - B[i] + cht.query_q(x[i]);
-            }
-        }
-        printf("%lld\n", dp[N-1][K]);
+        ll C = computeC();
+        int k = computeDp(C);
+        printf("%lld\n", dp[N] - C*K);
     }
     return 0;
 }
