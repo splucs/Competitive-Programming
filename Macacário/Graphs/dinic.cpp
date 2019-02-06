d71f8f7308bea23e7ea7deb4cd350920f39f4199
splucs <lucas.fra.oli18@gmail.com>
2019-02-06 19:06:41 -0200

    updated dinic

--- Macacário/Graphs/dinic.cpp
+++ Macacário/Graphs/dinic.cpp
@@ -9,8 +9,8 @@ using namespace std;
  * Dinic's Algorithm - O(EV^2)
  */
 
-int ned, first[MAXN], work[MAXN];
-int cap[MAXM], to[MAXM], nxt[MAXM], dist[MAXN];
+int ned, first[MAXN], work[MAXN], dist[MAXN], q[MAXN];
+int cap[MAXM], to[MAXM], nxt[MAXM];
 
 void init() {
    memset(first, -1, sizeof first);
@@ -18,22 +18,21 @@ void init() {
 }
 
 void add(int u, int v, int f) {
-    to[ned] = v, cap[ned] = f;
-    nxt[ned] = first[u];
-    first[u] = ned++;
-    
-    to[ned] = u, cap[ned] = 0;
-    nxt[ned] = first[v];
-    first[v] = ned++;
+	to[ned] = v, cap[ned] = f;
+	nxt[ned] = first[u];
+	first[u] = ned++;
+	
+	to[ned] = u, cap[ned] = 0;
+	nxt[ned] = first[v];
+	first[v] = ned++;
 }
 
-int dfs(int u, int f, int s, int t) {
+int dfs(int u, int f, int t) {
 	if (u == t) return f;
-	int v, df;
-	for(int & e = work[u]; e!=-1; e = nxt[e]) {
-        v = to[e];
+	for(int &e = work[u]; e != -1; e = nxt[e]) {
+		int v = to[e];
 		if (dist[v] == dist[u] + 1 && cap[e] > 0) {
-			df = dfs(v, min(f, cap[e]), s, t);
+			int df = dfs(v, min(f, cap[e]), t);
 			if (df > 0) {
 				cap[e] -= df;
 				cap[e^1] += df;
@@ -45,17 +44,17 @@ int dfs(int u, int f, int s, int t) {
 }
 
 bool bfs(int s, int t) {
-	int u, v;
 	memset(&dist, -1, sizeof dist);
 	dist[s] = 0;
-	queue<int> q; q.push(s);
-	while (!q.empty()) {
-		u = q.front(); q.pop();
+	int st = 0, en = 0;
+	q[en++] = s;
+	while (en > st) {
+		int u = q[st++];
 		for(int e = first[u]; e!=-1; e = nxt[e]) {
-			v = to[e];
+			int v = to[e];
 			if (dist[v] < 0 && cap[e] > 0) {
 				dist[v] = dist[u] + 1;
-				q.push(v);
+				q[en++] = v;
 			}
 		}
 	}
@@ -66,7 +65,7 @@ int dinic(int s, int t) {
 	int result = 0, f;
 	while (bfs(s, t)) {
 		memcpy(work, first, sizeof work);
-		while (f = dfs(s, INF, s, t)) result += f;
+		while (f = dfs(s, INF, t)) result += f;
 	}
 	return result;
 }
