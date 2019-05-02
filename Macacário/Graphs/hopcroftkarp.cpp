7d84a84d72e0dea9bca50ab6715e3efa09c59443
splucs <lucas.fra.oli18@gmail.com>
2019-05-02 22:38:08 +0100

    converted spaces to tabs

--- Macacário/Graphs/hopcroftkarp.cpp
+++ Macacário/Graphs/hopcroftkarp.cpp
@@ -15,54 +15,54 @@ int m, n;
 //Vertices enumerados de 1 a m em U e de 1 a n em V!!!!
 
 bool bfs() {
-    queue<int> q;
-    for (int u = 1; u <= m; u++) {
-        if (pairU[u] == 0) {
-            dist[u] = 0; q.push(u);
-        }
-        else dist[u] = INF;
-    }
-    dist[0] = INF;
-    while (!q.empty()) {
-        int u = q.front(); q.pop();
-        if (dist[u] >= dist[0]) continue;
-        for (int i = 0; i < (int)adjU[u].size(); i++) {
-            int v = adjU[u][i];
-            if (dist[pairV[v]] == INF) {
-                dist[pairV[v]] = dist[u] + 1;
-                q.push(pairV[v]);
-            }
-        }
-    }
-    return (dist[0] != INF);
+	queue<int> q;
+	for (int u = 1; u <= m; u++) {
+		if (pairU[u] == 0) {
+			dist[u] = 0; q.push(u);
+		}
+		else dist[u] = INF;
+	}
+	dist[0] = INF;
+	while (!q.empty()) {
+		int u = q.front(); q.pop();
+		if (dist[u] >= dist[0]) continue;
+		for (int i = 0; i < (int)adjU[u].size(); i++) {
+			int v = adjU[u][i];
+			if (dist[pairV[v]] == INF) {
+				dist[pairV[v]] = dist[u] + 1;
+				q.push(pairV[v]);
+			}
+		}
+	}
+	return (dist[0] != INF);
 }
 
 bool dfs(int u) {
-    if (u == 0) return true;
-    for (int i = 0; i < (int)adjU[u].size(); i++) {
-        int v = adjU[u][i];
-        if (dist[pairV[v]] == dist[u]+1) {
-            if (dfs(pairV[v])) {
-                pairV[v] = u; pairU[u] = v;
-                return true;
-            }
-        }
-    }
-    dist[u] = INF;
-    return false;
+	if (u == 0) return true;
+	for (int i = 0; i < (int)adjU[u].size(); i++) {
+		int v = adjU[u][i];
+		if (dist[pairV[v]] == dist[u]+1) {
+			if (dfs(pairV[v])) {
+				pairV[v] = u; pairU[u] = v;
+				return true;
+			}
+		}
+	}
+	dist[u] = INF;
+	return false;
 }
 
 int hopcroftKarp() {
-    memset(&pairU, 0, sizeof pairU);
-    memset(&pairV, 0, sizeof pairV);
-    int result = 0;
-    while (bfs()) {
-        for (int u=1; u<=m; u++) {
-            if (pairU[u]==0 && dfs(u))
-                result++;
-        }
-    }
-    return result;
+	memset(&pairU, 0, sizeof pairU);
+	memset(&pairV, 0, sizeof pairV);
+	int result = 0;
+	while (bfs()) {
+		for (int u=1; u<=m; u++) {
+			if (pairU[u]==0 && dfs(u))
+				result++;
+		}
+	}
+	return result;
 }
 
 /*
