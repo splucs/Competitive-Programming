7cad6edfbd866d1967ea0a199d38fe4870f9a16c
Lucas França de Oliveira <lucasfdo@palantir.com>
2023-05-15 17:13:24 -0700

    leetcode

---
+++ Leetcode/24.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (!head || !head->next) return head;
+        ListNode *third = swapPairs(head->next->next);
+        ListNode *second = head->next;
+        head->next = third;
+        second->next = head;
+        return second;
+    }
+};
