fb036ebc6120b2b876ae7e263373f3499b10d7d6
Lucas França de Oliveira <lucasfdo@palantir.com>
2023-06-05 12:16:37 -0700

    leetcode

--- Leetcode/1232.cpp
+++ Leetcode/1232.cpp
@@ -12,22 +12,20 @@ int inline cross(point p1, point p2) {
 	return p1.x*p2.y - p1.y*p2.x;
 }
 
-bool inline collinear(point p, point q, point r) {
-	return cross(p-q, r-p) == 0;
-}
-
 class Solution {
 public:
     bool checkStraightLine(vector<vector<int>>& coordinates) {
         int n = coordinates.size();
         point p1(coordinates[0][0], coordinates[0][1]);
         point p2(coordinates[1][0], coordinates[1][1]);
+        point dp = p2-p1;
         for (int i = 2; i < n; i++) {
             point p3(coordinates[i][0], coordinates[i][1]);
-            if (!collinear(p1, p2, p3)) {
+            if (cross(dp, p3-p2)) {
                 return false;
             }
         }
         return true;
     }
 };
+
