8d458c4018982a709fbb3d74f29656b0be46f321
Lucas França de Oliveira <lucasfdo@palantir.com>
2023-07-29 11:30:58 -0700

    leetcode

---
+++ Leetcode/808.cpp
@@ -0,0 +1,44 @@
+class Solution {
+private:
+    int di[4] = {-4, -3, -2, -1};
+    int dj[4] = {0, -1, -2, -3};
+    const int MAXN = 175;
+public:
+    double soupServings(int n) {
+        if (n%25 != 0) {
+            n += 25 - (n%25);
+        }
+        n /= 25;
+
+        // Conjecture: the answer converges and MAXN is enough precision
+        if (n > MAXN) n = MAXN;
+
+        // dp[i][j] = answer given I have i*25ml of A and j*25ml of B
+        vector<vector<double>> dp(n+1, vector<double>(n+1));
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                double &ans = dp[i][j];
+
+                // Base cases
+                if (i == 0 || j == 0) {
+                    if (i == j) ans = 0.5;
+                    else if (i == 0) ans = 1;
+                    else ans = 0;
+                    continue;
+                }
+
+                // Recursion
+                for (int k = 0; k < 4; k++) {
+                    int ni = i + di[k];
+                    if (ni < 0) ni = 0;
+                    int nj = j + dj[k];
+                    if (nj < 0) nj = 0;
+                    ans += dp[ni][nj];
+                }
+                ans /= 4.0;
+            }
+        }
+        return dp[n][n];
+    }
+};
