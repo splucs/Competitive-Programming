84e2fe7e9ee6b4eae016094261ea1263c04b011b
Lucas França de Oliveira <lucasfdo@palantir.com>
2023-08-11 16:31:47 -0700

    leetcode

--- Leetcode/518.cpp
+++ Leetcode/518.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        int n = coins.size();
         vector<int> dp(amount+1, 0);
         dp[0] = 1;
-        for (int j = 0; j < n; j++) {
-            int coin = coins[j];
+        for (int coin : coins) {
             for (int i = coin; i <= amount; i++) {
                 dp[i] += dp[i-coin];
             }
