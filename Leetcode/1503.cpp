bad16207c71143a9369513da64f0dc52ec53290c
Lucas França de Oliveira <lucasfdo@palantir.com>
2023-11-03 17:39:15 -0700

    leetcode

---
+++ Leetcode/1503.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        int ans = 0;
+        for (int x : left) {
+            ans = max(ans, x);
+        }
+        for (int x : right) {
+            ans = max(ans, n-x);
+        }
+        return ans;
+    }
+};
